1a1a82e16b83a176ee7146777bcd42bb42df807c
Subeesh Kumar Padikkale Kandathil <subeesh99@yahoo.com>
2024-02-19 23:33:15 -0600

    Space cleanup

--- src/main/java/com/rewards/program/ProgramApplication.java
+++ src/main/java/com/rewards/program/ProgramApplication.java
@@ -2,12 +2,9 @@ package com.rewards.program;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ProgramApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramApplication.class, args);
 	}
-
 }
